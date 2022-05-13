d250ce740ebf4f70f40d9c7ddd844a7734464cff
najasoft <nadiri@najasoft.com>
2022-05-13 19:14:00 +0100

    Delete Untitled-1.java

--- mvc-thymeleaf/src/main/resources/templates/Untitled-1.java
+++
@@ -1,9 +0,0 @@
-/*
-	 * @GetMapping("/login") public String login(Model model) { User u=new User();
-	 * model.addAttribute("user",u); return "login"; }
-	 * 
-	 * 
-	 * @PostMapping("/login") public String login(Model model) { User u=new User();
-	 * model.addAttribute("user",u); return "login"; }
-	 */
-	*/
