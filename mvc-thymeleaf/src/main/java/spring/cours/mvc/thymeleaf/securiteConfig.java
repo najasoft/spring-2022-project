fae01aa2bfe7257b104dce434350eb4be12af532
najasoft <nadiri@najasoft.com>
2022-05-13 19:42:44 +0100

    V2

--- mvc-thymeleaf/src/main/java/spring/cours/mvc/thymeleaf/securiteConfig.java
+++ mvc-thymeleaf/src/main/java/spring/cours/mvc/thymeleaf/securiteConfig.java
@@ -1,26 +1,25 @@
 package spring.cours.mvc.thymeleaf;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import spring.cours.mvc.thymeleaf.services.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class securiteConfig extends WebSecurityConfigurerAdapter {
 
-@Autowired
+	@Autowired
 	public void configureInMemoryUsers(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("u1").password("{noop}p1").roles("ADMIN", "USER").and().withUser("u2")
-				.password("{noop}p2").roles("USER").and().withUser("u3").password("{noop}p3").roles("USER");
-			
+		auth.inMemoryAuthentication()
+		.withUser("u1").password("{noop}p1").roles("ADMIN", "USER")
+		.and()
+		.withUser("u2").password("{noop}p2").roles("USER")
+		.and()
+		.withUser("u3").password("{noop}p3").roles("ADMIN");
+
 	}
 
 	@Override
